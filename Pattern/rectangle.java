6738fa53981a9a16fb7c55661bd55d4cca356331
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2022-02-21 01:52:51 +0530

    Create rectangle.java

---
+++ Pattern/rectangle.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+    
+    //Rectangle Pattern.  
+	    Scanner sc = new Scanner(System.in);
+	    
+	    //different number of rows and different number of columns..
+	     //you can take rows and columns input from the user..
+	     int r = sc.nextInt();
+	     int c = sc.nextInt();
+	    
+	    int i,j;
+	    for(i=0;i<r;i++)
+	    {
+	        for(j=0;j<c;j++)
+	        {
+	            System.out.print("* ");
+	        }
+	        System.out.println(); //for changing line
+	    }
+	}
+}
