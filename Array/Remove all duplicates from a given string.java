c0d0bb841943dcf9071a212637250c9e9c53fb17
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2022-05-18 15:00:38 +0530

    Create Remove all duplicates from a given string.java

---
+++ Array/Remove all duplicates from a given string.java
@@ -0,0 +1,52 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String str = br.readLine().trim();
+
+            String ans = new Solution().removeDuplicates(str);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution 
+{
+    String removeDuplicates(String str) 
+    {
+        
+        // code here
+        List<Character> list = new ArrayList<>();
+        
+        int i;
+        for(i=0;i<str.length();i++)
+        {
+            if(!list.contains(str.charAt(i)))
+            {
+                list.add(str.charAt(i));
+            }
+        }
+        String res = "";
+        for(i=0;i<list.size();i++)
+        {
+            res = res + Character.toString(list.get(i));
+        }
+        return res;
+    }
+}
