be0d09881348daebd9bcf9d36894966fffd89737
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2022-05-19 15:33:07 +0530

    Create Single Number.java

---
+++ Array/Single Number.java
@@ -0,0 +1,65 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            int n = Integer.parseInt(br.readLine().trim());
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().getSingle(arr, n);
+            System.out.println(ans);
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+class Solution 
+{
+    int getSingle(int arr[], int n) 
+    {
+        // code here
+        
+        //using hash map..
+        Map<Integer,Integer> mp = new HashMap<>();
+        int i;
+        for(i=0;i<n;i++)
+        {
+            if(mp.containsKey(arr[i]))
+            {
+                mp.put(arr[i],mp.get(arr[i])+1);
+            }
+            else
+            {
+                mp.put(arr[i],1);
+            }
+        }
+        int value=0;
+        //traverse the map..
+        for(Map.Entry<Integer,Integer> entry : mp.entrySet())
+        {
+            if(entry.getValue()%2!=0)
+            {
+                 value = entry.getKey();
+                 break;
+            }
+        }
+        return value;
+    }
+}
