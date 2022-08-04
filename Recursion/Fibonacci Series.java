be66f0e85431ed018ca0c2e4b4d17f38f5a889c0
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2022-08-05 01:04:49 +0530

    Create Fibonacci Series.java

---
+++ Recursion/Fibonacci Series.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();//Enter the number..
+        
+        //print the fibonacci series till the nth term..
+        int a=0,b=1;
+        System.out.println(a);
+        System.out.println(b);
+        printFibonacci(a,b,n-2); 
+	}
+	public static void printFibonacci(int a,int b,int n)
+	{   
+	    if(n==0)
+	    {
+	        return;
+	    }
+	    int sum = a+b;
+	    System.out.println(sum);
+        printFibonacci(b,sum,n-1);
+	}   
+}
