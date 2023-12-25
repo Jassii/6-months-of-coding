42d90c4e538fcf15993ccfc8ec180bcb1e9eb9d6
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2023-12-25 23:46:35 +0530

    Create Middle Of Linked List.java

---
+++ CodingNinjaStudio/Middle Of Linked List.java
@@ -0,0 +1,57 @@
+/****************************************************************
+
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node findMiddle(Node head)
+    {
+        // Write your code here.
+        if(head==null || head.next==null)
+        {
+            return head;
+        }
+
+        int n = 1;
+        Node curr = head;
+        while(curr.next!=null)
+        {
+            curr = curr.next;
+            n++;
+        }
+
+        int mid = (n/2)+1;  //3
+        curr = head; 
+        
+
+        while(mid>1)
+        {
+            curr = curr.next;
+            mid--;
+        }
+        return curr;
+    }
+}
