68d50b4d6c5318c0a31f969d1628d250cddce012
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2024-01-04 00:18:11 +0530

    Update Distinct absolute array elements.java

--- GeeksForGeeks/Distinct absolute array elements.java
+++ GeeksForGeeks/Distinct absolute array elements.java
@@ -3,23 +3,35 @@ class Solution
     int distinctCount(int[] arr, int n) 
     {
         // code here
-        
-        //it is better to take HashSet..(TC -> O(1))
-        HashSet<Integer> hset = new HashSet<>();
+
+         HashSet<Integer> hset = new HashSet<>();
         int i,count=0;
+        //It is a set, so no distint elements will be there
         for(i=0;i<n;i++)
         {
-            if(hset.contains(Math.abs(arr[i])))
-            {
-                continue;
-            }
-            else
-            {
-                hset.add(Math.abs(arr[i]));
-                count++;
-            }
+            hset.add(Math.abs(arr[i]));
         }
         
-        return count;
+        return hset.size();
+
+
+        
+        //it is better to take HashSet..(TC -> O(1))
+        // HashSet<Integer> hset = new HashSet<>();
+        // int i,count=0;
+        // for(i=0;i<n;i++)
+        // {
+        //     if(hset.contains(Math.abs(arr[i])))
+        //     {
+        //         continue;
+        //     }
+        //     else
+        //     {
+        //         hset.add(Math.abs(arr[i]));
+        //         count++;
+        //     }
+        // }
+        
+        // return count;
     }
 }
